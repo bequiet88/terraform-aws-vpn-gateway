bb461d96b139cf248864cfc41a0897336936e409
Jochen Hülß <jhuelss@NUEL-C9HZ562-DEBIAN.gfk.com>
2018-02-15 14:39:11 +0100

    Adding Tunnel1/2 Inside CIDR and Preshared Key

--- main.tf
+++ main.tf
@@ -1,13 +1,19 @@
 resource "aws_vpn_connection" "default" {
-  count = "${var.create_vpn_connection ? 1 : 0}"
+  count                 = "${var.create_vpn_connection ? 1 : 0}"
 
-  vpn_gateway_id      = "${var.vpn_gateway_id}"
-  customer_gateway_id = "${var.customer_gateway_id}"
-  type                = "ipsec.1"
+  vpn_gateway_id        = "${var.vpn_gateway_id}"
+  customer_gateway_id   = "${var.customer_gateway_id}"
+  type                  = "ipsec.1"
 
-  static_routes_only = "${var.vpn_connection_static_routes_only}"
+  static_routes_only    = "${var.vpn_connection_static_routes_only}"
 
-  tags = "${merge(
+  tunnel1_inside_cidr   = "${var.tunnel1_inside_cidr}"
+  tunnel2_inside_cidr   = "${var.tunnel2_inside_cidr}"
+
+  tunnel1_preshared_key = "${var.tunnel1_preshared_key}"
+  tunnel2_preshared_key = "${var.tunnel2_preshared_key}"
+
+  tags                  = "${merge(
     var.tags,
     map(
       "Name", "VPN Connection between VPC ${var.vpc_id} and Customer Gateway ${var.customer_gateway_id}"
@@ -21,14 +27,14 @@ resource "aws_vpn_gateway_attachment" "default" {
 }
 
 resource "aws_vpn_gateway_route_propagation" "private_subnets_vpn_routing" {
-  count = "${var.vpc_subnet_route_table_count}"
+  count          = "${var.vpc_subnet_route_table_count}"
 
   vpn_gateway_id = "${var.vpn_gateway_id}"
   route_table_id = "${element(var.vpc_subnet_route_table_ids, count.index)}"
 }
 
 resource "aws_vpn_connection_route" "default" {
-  count = "${var.create_vpn_connection ? (var.vpn_connection_static_routes_only ? length(var.vpn_connection_static_routes_destinations) : 0) : 0}"
+  count                  = "${var.create_vpn_connection ? (var.vpn_connection_static_routes_only ? length(var.vpn_connection_static_routes_destinations) : 0) : 0}"
 
   vpn_connection_id      = "${element(split(",", (var.create_vpn_connection ? join(",", aws_vpn_connection.default.*.id) : "")), 0)}"
   destination_cidr_block = "${element(var.vpn_connection_static_routes_destinations, count.index)}"
