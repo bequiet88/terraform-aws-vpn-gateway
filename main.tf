975057140ec86f31b42c1a7d976c00626bfa1fb8
Jochen Hülß <jhuelss@NUEL-C9HZ562-DEBIAN.gfk.com>
2018-02-15 15:00:25 +0100

    Workaround for missing if

--- main.tf
+++ main.tf
@@ -1,5 +1,66 @@
+### Default
 resource "aws_vpn_connection" "default" {
-  count                 = "${var.create_vpn_connection ? 1 : 0}"
+  count               = "${var.create_vpn_connection ? 1 : 0}"
+
+  vpn_gateway_id      = "${var.vpn_gateway_id}"
+  customer_gateway_id = "${var.customer_gateway_id}"
+  type                = "ipsec.1"
+
+  static_routes_only  = "${var.vpn_connection_static_routes_only}"
+
+  tags                = "${merge(
+    var.tags,
+    map(
+      "Name", "VPN Connection between VPC ${var.vpc_id} and Customer Gateway ${var.customer_gateway_id}"
+    )
+  )}"
+}
+
+### Tunnel Inside CIDR only
+resource "aws_vpn_connection" "default" {
+  count                 = "${var.create_vpn_connection && length(var.tunnel1_inside_cidr) > 0 && length(var.tunnel2_inside_cidr) > 0 ? 1 : 0}"
+
+  vpn_gateway_id        = "${var.vpn_gateway_id}"
+  customer_gateway_id   = "${var.customer_gateway_id}"
+  type                  = "ipsec.1"
+
+  static_routes_only    = "${var.vpn_connection_static_routes_only}"
+
+  tunnel1_inside_cidr   = "${var.tunnel1_inside_cidr}"
+  tunnel2_inside_cidr   = "${var.tunnel2_inside_cidr}"
+
+  tags                  = "${merge(
+    var.tags,
+    map(
+      "Name", "VPN Connection between VPC ${var.vpc_id} and Customer Gateway ${var.customer_gateway_id}"
+    )
+  )}"
+}
+
+### Preshared Key only
+resource "aws_vpn_connection" "default" {
+  count                 = "${var.create_vpn_connection && length(var.tunnel1_preshared_key) > 0 && length(var.tunnel2_preshared_key) > 0 ? 1 : 0}"
+
+  vpn_gateway_id        = "${var.vpn_gateway_id}"
+  customer_gateway_id   = "${var.customer_gateway_id}"
+  type                  = "ipsec.1"
+
+  static_routes_only    = "${var.vpn_connection_static_routes_only}"
+
+  tunnel1_preshared_key = "${var.tunnel1_preshared_key}"
+  tunnel2_preshared_key = "${var.tunnel2_preshared_key}"
+
+  tags                  = "${merge(
+    var.tags,
+    map(
+      "Name", "VPN Connection between VPC ${var.vpc_id} and Customer Gateway ${var.customer_gateway_id}"
+    )
+  )}"
+}
+
+### Tunnel Inside CIDR and Preshared Key
+resource "aws_vpn_connection" "default" {
+  count                 = "${var.create_vpn_connection && length(var.tunnel1_inside_cidr) > 0 && length(var.tunnel2_inside_cidr) > 0 && length(var.tunnel1_preshared_key) > 0 && length(var.tunnel2_preshared_key) > 0 ? 1 : 0}"
 
   vpn_gateway_id        = "${var.vpn_gateway_id}"
   customer_gateway_id   = "${var.customer_gateway_id}"
@@ -20,6 +81,7 @@ resource "aws_vpn_connection" "default" {
     )
   )}"
 }
+
 
 resource "aws_vpn_gateway_attachment" "default" {
   vpc_id         = "${var.vpc_id}"
